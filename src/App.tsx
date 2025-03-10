c355776c378ee8a67c33027182df682701ee7d84
Daniel Paulino <danielpaulino87@hotmail.es>
2025-03-10 03:32:34 -0400

    add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import { useEffect } from "react";
 import ReactGA from "react-ga4";
 import { Mail } from 'lucide-react';
+import favicon from './assets/favicon-196x196.png';
 
 function App() {
   const currentYear = new Date().getFullYear();
+  
   useEffect(() => {
-    ReactGA.initialize("G-W2FPRJ6YES"); 
+    ReactGA.initialize("G-W2FPRJ6YES");
     ReactGA.send("pageview");
   }, []);
+  
   return (
-    <div className="min-h-screen bg-black flex flex-col items-center justify-center text-white">
+    <div className="min-h-screen bg-[#212121] flex flex-col items-center justify-center text-white">
       <div className="text-center">
-        <div className="text-4xl md:text-6xl mb-2 tracking-wider">
-          {'</>'}
+        <div className="text-4xl md:text-6xl mb-2 tracking-wider flex justify-center items-center">
+          <img 
+            src={favicon} 
+            alt="Favicon" 
+            className="w-16 h-16 md:w-24 md:h-24 object-contain" 
+          />
         </div>
         <h1 className="text-5xl md:text-7xl font-light tracking-wider flex flex-col gap-2">
           <span>CANEY</span>
